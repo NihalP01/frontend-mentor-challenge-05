326187082a02d2d1511d819e56f98e1d91963d9c
Nihalp01 <nihalburagohain02@gmail.com>
2023-11-09 01:16:51 +0530

    modal has been implemented

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const errorMsg = document.getElementById('error-msg');
 const email = document.getElementById('email');
+const modal = document.getElementById('modal');
+const form = document.getElementById('form');
+const userEmail = document.getElementById('user-mail');
 
-const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const btnDismiss = document.getElementById('btn-dismiss');
+const emailPattern =
+  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const btnSubmit = document.getElementById('btn-submit');
 
 btnSubmit.addEventListener('click', () => {
@@ -12,8 +17,16 @@ btnSubmit.addEventListener('click', () => {
     email.style.backgroundColor = 'hsla(11, 100%, 63%, 15%)';
   } else {
     errorMsg.style.display = 'none';
+    form.style.display = 'none';
     email.style.color = '';
     email.style.border = '';
     email.style.backgroundColor = '';
+    modal.style.display = 'flex';
+    userEmail.textContent = email.value;
   }
+});
+
+btnDismiss.addEventListener('click', () => {
+  form.style.display = 'flex';
+  modal.style.display = 'none';
 });
